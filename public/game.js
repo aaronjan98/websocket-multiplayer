5bbb46696db1c150220b861d082e8d5cc5c58613
aaronjan <aaronjan98@gmail.com>
2020-09-14 11:02:01 -0700

    URL's scheme must be either 'ws' or 'wss'. 'https' is not allowed.

--- public/game.js
+++ public/game.js
@@ -39,7 +39,7 @@ let protocol = location.protocol.replace(/^http/, 'ws').replace(/^https/, 'ws');
 console.log('location: ', location);
 // console.log('HOST: ', HOST);
 // let ws = new WebSocket(HOST);
-let ws = new WebSocket(`https://websocket-multiplayer-pong.herokuapp.com`);
+let ws = new WebSocket(`${protocol}://websocket-multiplayer-pong.herokuapp.com`);
 
 // HTML elements
 const btnCreate = document.getElementById('btnCreate');
