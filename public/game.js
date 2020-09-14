f81cd8ddd462daca2d844652e4eed1fefba5bad7
aaronjan <aaronjan98@gmail.com>
2020-09-14 12:51:56 -0700

    upgraded dyno to 1

--- public/game.js
+++ public/game.js
@@ -32,7 +32,6 @@ let playerColor = 'blue';
 
 let port = parseInt(location.port);
 let protocol = location.protocol.replace(/^http/, 'ws').replace(/^https/, 'ws');
-// let protocol = location.href.replace('http://', 'ws://').replace('https://', 'wss://') + '/to/ws';
 // let hostname = location.hostname;
 // let HOST = `${protocol}//${hostname}:${port}`;
 let HOST = location.origin.replace(/^http/, 'ws').replace(/^https/, 'ws');
@@ -40,7 +39,7 @@ let HOST = location.origin.replace(/^http/, 'ws').replace(/^https/, 'ws');
 console.log('location: ', location);
 // console.log('HOST: ', HOST);
 // let ws = new WebSocket(HOST);
-let ws = new WebSocket(HOST);
+let ws = new WebSocket(`${protocol}//websocket-multiplayer-pong.herokuapp.com:${port}`);
 
 // HTML elements
 const btnCreate = document.getElementById('btnCreate');
